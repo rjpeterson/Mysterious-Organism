0508eae29462d43ac4033a2c6c0d84f2109bb067
rjpeterson <r.james.peterson@gmail.com>
2019-10-09 22:02:28 +0800

    remove original functions, add PAequor class

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
-// Returns a random DNA base
-const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
-
-// Returns a random single stand of DNA containing 15 bases
-const mockUpStrand = () => {
-  const newStrand = [];
-  for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+class PAequor {
+  constructor (specimenNum, dnaSeq) {
+    this.dnaBases = ['A', 'T', 'C', 'G']
+    this.dnaLength = 15
+    this.specimenNum = specimenNum
+    this.dna = this.mockUpStrand()
   }
-  return newStrand;
-};
 
+  // Returns a random number between two paramters
+  returnRandNum (low, high) {
+    const range = high - low
+    return Math.floor(Math.random() * range) + low
+  }
 
+  // Returns a random DNA base
+  returnRandBase () {
+    return this.dnaBases[this.returnRandNum(0, 3)]
+  }
 
+  // Returns a random single strand of DNA
+  mockUpStrand () {
+    const newStrand = []
+    for (let i = 0; i < this.dnaLength; i++) {
+      newStrand.push(this.returnRandBase())
+    }
+    return newStrand
+  }
 
+  // Randomly select a base in this.dna and change to a random other base
+  mutate () {
+    // Choose random index from this.dna
+    const indexToChange = this.returnRandNum(0, this.dnaLength)
+    console.log(`indexToChange = ${indexToChange}`)
+    // Identify corresponding base
+    const oldBaseIndex = this.dnaBases.indexOf(this.dna[indexToChange])
+    console.log(`Old Base = ${this.dnaBases[oldBaseIndex]}`)
+    // Choose random new base
+    const newBaseIndex = (oldBaseIndex + this.returnRandNum(1, 3)) % 4
+    console.log(`New Base = ${this.dnaBases[newBaseIndex]}`)
+    // Replace old base with new
+    this.dna[indexToChange] = this.dna[newBaseIndex]
+  }
+}
 
-
-
-
+const test = new PAequor(0)
+console.log(test.dna)
+test.mutate()
+console.log(test.dna)
